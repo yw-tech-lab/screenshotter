0716a4799c68204012ac825b830f5e2d5c1130b3
Sarah <svanwart@unca.edu>
2025-04-06 12:35:23 -0400

    updates

---
+++ README.md
@@ -0,0 +1,7 @@
+# To use this script
+
+1. Download the code (git clone ...)
+2. Navigate to the directory you just cloned on the command line
+3. Install the dependencies (`npm install`)
+4. Run the script (`node main.mjs`)
+5. Change the URLs and screen dimensions as needed.
---
+++ aux.mjs
@@ -0,0 +1,20 @@
+const urls = [
+    "https://theauxevanston.com/",
+    "https://theauxevanston.com/who-we-are/",
+    "https://theauxevanston.com/what-we-do/",
+    "https://theauxevanston.com/meet-our-tenants/",
+    "https://theauxevanston.com/open-spaces/",
+    "https://theauxevanston.com/milestones/",
+    "https://theauxevanston.com/news/",
+    "https://theauxevanston.com/contact/",
+    "https://theauxevanston.com/invest-and-donate/",
+    "https://theauxevanston.com/faqs/",
+    "https://theauxevanston.com/meet-our-tenants/the-aux-wellness-collective/",
+    "https://theauxevanston.com/meet-our-tenants/embrace-your-crown/",
+    "https://theauxevanston.com/meet-our-tenants/the-laundry-cafe/",
+    "https://theauxevanston.com/meet-our-tenants/our-unseen-roots-podcast-studio/",
+    "https://theauxevanston.com/2023/06/03/tbf-diagnostic/",
+    "https://www.facebook.com/theauxevanston/",
+    "https://www.instagram.com/theauxevanston/",
+];
+export default urls;
